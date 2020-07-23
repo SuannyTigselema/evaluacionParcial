06d099591b5a9f82ce40a8e7f6649885b5d3fce2
SuannyTigselema <66803060+SuannyTigselema@users.noreply.github.com>
2020-07-23 17:03:13 -0500

    Crear activitys

---
+++ app/src/main/java/com/example/evaluacionparcial/infoPaisActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluacionparcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class infoPaisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_pais);
+    }
+}
