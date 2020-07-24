d873fb6ea7fe93419e016e2a957c931f0a3ef673
SuannyTigselema <66803060+SuannyTigselema@users.noreply.github.com>
2020-07-23 23:53:59 -0500

    Clase detallePais

--- app/src/main/java/com/example/evaluacionparcial/Models/detallePais.java
+++ app/src/main/java/com/example/evaluacionparcial/Models/detallePais.java
@@ -1,5 +1,69 @@
 package com.example.evaluacionparcial.Models;
 
-public class detallePais {
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class detallePais {
+    public String name;
+    public String region;
+    public String nativeName;
+    public String numericCode;
+    public String language;
+    public String band;
+
+    public detallePais(JSONObject a) throws JSONException {
+        this.name = a.getString("name").toString();
+        this.region = region;
+        this.nativeName = nativeName;
+        this.numericCode = numericCode;
+        this.language = language;
+        this.band =  "https://restcountries.eu/rest/v2/alpha/"+;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getNativeName() {
+        return nativeName;
+    }
+
+    public void setNativeName(String nativeName) {
+        this.nativeName = nativeName;
+    }
+
+    public String getNumericCode() {
+        return numericCode;
+    }
+
+    public void setNumericCode(String numericCode) {
+        this.numericCode = numericCode;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
 }
